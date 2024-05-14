e1993983082f38d3530ba35895ac3d6d7870c301
rebpacquiao <reb.pacquiao@gmail.com>
2024-05-14 20:15:19 +0800

    Refactor types package, chat, brand, and product to remove unused code

--- src/src/pages/Dashboard/ECommerce.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import DefaultLayout from '../../layout/DefaultLayout';
-
-const ECommerce: React.FC = () => {
-  return <DefaultLayout>test</DefaultLayout>;
-};
-
-export default ECommerce;
