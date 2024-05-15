0abd8e69e40f633757052662cbbf3153dea6e01e
rebpacquiao <reb.pacquiao@gmail.com>
2024-05-15 20:09:51 +0800

    Refactor CSS styles for better readability and user experience

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import { useEffect, useState } from 'react';
 import { Route, Routes, useLocation } from 'react-router-dom';
-import Login from './pages/Login';
 
 import Loader from './common/Loader';
 import PageTitle from './components/PageTitle';
 import Dashboard from './pages/Dashboard/Dashboard';
+import Login from './pages/Login';
 import { AuthProvider } from './components/AuthProvider/AuthProvider';
 
 function App() {
@@ -22,20 +22,22 @@ function App() {
   return loading ? (
     <Loader />
   ) : (
-    <AuthProvider>
-      <Routes>
-        <Route path="/" index element={<Login />} />
-        <Route
-          path="/dashboard"
-          element={
-            <>
-              <PageTitle title="LandConnex | Dashboard" />
-              <Dashboard />
-            </>
-          }
-        />
-      </Routes>
-    </AuthProvider>
+    <>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" index element={<Login />} />
+          <Route
+            path="/dashboard"
+            element={
+              <>
+                <PageTitle title="LandConnex | Dashboard" />
+                <Dashboard />
+              </>
+            }
+          />
+        </Routes>
+      </AuthProvider>
+    </>
   );
 }
 
