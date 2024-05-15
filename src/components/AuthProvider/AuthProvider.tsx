3a657ab7da26b9f89b02a6ad52a090979c30ef2d
rebpacquiao <reb.pacquiao@gmail.com>
2024-05-15 17:05:47 +0800

    feat: Add snackbar for displaying invalid credentials in AuthProvider

--- src/components/AuthProvider/AuthProvider.tsx
+++ src/components/AuthProvider/AuthProvider.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { AuthContext } from '../../contexts/AuthContext';
+import Snackbar from '@mui/material/Snackbar';
+import Alert from '@mui/material/Alert';
 
 interface AuthProviderProps {
   children: React.ReactNode;
@@ -7,15 +9,16 @@ interface AuthProviderProps {
 
 export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [openSnackbar, setOpenSnackbar] = useState(false);
 
   const handleLogin = (email: string, password: string) => {
     const dummyEmail = 'user@example.com';
-    const dummyPassword = 'password';
+    const dummyPassword = 'landConnex';
 
     if (email === dummyEmail && password === dummyPassword) {
       setIsLoggedIn(true);
     } else {
-      alert('Invalid credentials');
+      setOpenSnackbar(true);
     }
   };
 
@@ -23,11 +26,36 @@ export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
     setIsLoggedIn(false);
   };
 
+  const handleCloseSnackbar = (
+    event?: React.SyntheticEvent,
+    reason?: string,
+  ) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpenSnackbar(false);
+  };
+
   return (
     <AuthContext.Provider
       value={{ isLoggedIn, onLogin: handleLogin, onLogout: handleLogout }}
     >
       {children}
+      <Snackbar
+        open={openSnackbar}
+        autoHideDuration={6000}
+        onClose={handleCloseSnackbar}
+        anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+      >
+        <Alert
+          onClose={handleCloseSnackbar}
+          severity="error"
+          sx={{ width: '100%' }}
+        >
+          Invalid credentials
+        </Alert>
+      </Snackbar>
     </AuthContext.Provider>
   );
 };
