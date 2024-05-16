575f310fc598e501cd7f158fcdeaf6730e5e986a
rebpacquiao <reb.pacquiao@gmail.com>
2024-05-16 09:19:09 +0800

    Refactor DropdownUser component to update navigation link

--- src/components/Header/DropdownUser.tsx
+++ src/components/Header/DropdownUser.tsx
@@ -44,7 +44,7 @@ const DropdownUser = () => {
         ref={trigger}
         onClick={() => setDropdownOpen(!dropdownOpen)}
         className="flex items-center gap-4"
-        to="#"
+        to="/dashboard"
       >
         <span className="hidden text-right lg:block">
           <span className="block text-sm font-medium text-black dark:text-white">
