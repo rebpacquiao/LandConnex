84f596a7fdf87515c80813b4fa1e76b52f580a76
rebpacquiao <reb.pacquiao@gmail.com>
2024-05-16 09:30:09 +0800

    Refactor App component to use AuthContext for authentication logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import { useEffect, useState } from 'react';
+import { useContext, useState, useEffect } from 'react';
 import { Route, Routes, useLocation } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 
 import Loader from './common/Loader';
 import PageTitle from './components/PageTitle';
 import Dashboard from './pages/Dashboard/Dashboard';
 import Login from './pages/Login';
 import { AuthProvider } from './components/AuthProvider/AuthProvider';
+
+import { AuthContext } from './contexts/AuthContext';
 
 function App() {
   const [loading, setLoading] = useState<boolean>(true);
@@ -22,23 +25,36 @@ function App() {
   return loading ? (
     <Loader />
   ) : (
-    <>
-      <AuthProvider>
-        <Routes>
-          <Route path="/" index element={<Login />} />
-          <Route
-            path="/dashboard"
-            element={
-              <>
-                <PageTitle title="LandConnex | Dashboard" />
-                <Dashboard />
-              </>
-            }
-          />
-        </Routes>
-      </AuthProvider>
-    </>
+    <AuthProvider>
+      <RoutesComponent />
+    </AuthProvider>
   );
 }
 
 export default App;
+
+const RoutesComponent = () => {
+  const { isLoggedIn } = useContext(AuthContext);
+
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={isLoggedIn ? <Navigate to="/dashboard" /> : <Login />}
+      />
+      <Route
+        path="/dashboard"
+        element={
+          isLoggedIn ? (
+            <>
+              <PageTitle title="LandConnex | Dashboard" />
+              <Dashboard />
+            </>
+          ) : (
+            <Navigate to="/" />
+          )
+        }
+      />
+    </Routes>
+  );
+};
